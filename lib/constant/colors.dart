6f842619c178004852b4c28707280930fbadc876
ayaazzz <ayaa64222@gmail.com>
2024-03-08 15:10:43 +0200

    Initial commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF212224);
+  static const secondaryColor = Color.fromARGB(90050, 6000, 5010, 4060);
+  static const secondary2Color = Color(0xFF26282D);
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,90 @@
+import 'package:calculator_app1/constant/colors.dart';
+import 'package:calculator_app1/provider/cal_provider.dart';
+import 'package:calculator_app1/screens/widgets_data.dart';
+import 'package:calculator_app1/widgets/textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:calculator_app1/widgets/cal_button.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.sizeOf(context).height;
+    const padding = EdgeInsets.symmetric(horizontal: 25, vertical: 30);
+    const decoration = BoxDecoration(
+        color: AppColors.primaryColor,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30)));
+
+    return Consumer<CalculatorProvider>(builder: (context, provider, _) {
+      return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text("Calculator App"),
+          backgroundColor: Colors.black,
+        ),
+        body: Column(
+          children: [
+            CustomTextField(
+              controller: provider.compController,
+            ),
+            const Spacer(),
+            Container(
+              height: screenHeight * 0.6,
+              width: double.infinity,
+              padding: padding,
+              decoration: decoration,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: List.generate(4, (index) => buttonList[index]),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children:
+                    List.generate(4, (index) => buttonList[index + 4]),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children:
+                    List.generate(4, (index) => buttonList[index + 8]),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: List.generate(
+                                  3, (index) => buttonList[index + 12]),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: List.generate(
+                                  3, (index) => buttonList[index + 15]),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const CalculateButton()
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    });
+  }
+}
